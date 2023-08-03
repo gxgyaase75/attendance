27fe5a4964e8db238d0f80132be265f9560b9b13
gxgyaase75 <gabrielgyaase@gmail.com>
2023-08-03 11:56:12 -0400

    added student.html and entries.html

--- server.js
+++ server.js
@@ -60,3 +60,86 @@ app.post('/addstudent', async (req, res) => {
   res.status(200).send("successfully added")
 
 })
+
+app.get('/getroster', async (req, res) => {
+
+ 
+
+  const { date, status } = req.query;
+
+  const dateRef = db.collection("attendance").doc(date);
+
+  const doc = await dateRef.get();
+
+  const students = [];
+
+  if (!doc.exists) {
+      return res.sendStatus(400)
+  }
+  const data = doc.data();
+  for (let k in data) {
+   if (data[k]["status"] == status) {
+       students.push(k);
+   }
+  }
+  res.status(200).send(students)
+})
+
+app.get('/data', (req, res) => {
+
+    db.collection("attendance")
+
+      .get()
+
+      .then(function(querySnapshot) {
+
+        var data = [];
+
+        querySnapshot.forEach(function(doc) {
+
+          data.push([doc.id,doc.data()]);
+
+          
+
+        });
+
+        console.log(data);
+
+        res.send(data);
+
+      })
+
+      .catch(function(error) {
+
+        console.log('Error getting Firestore data: ', error);
+
+        res.status(500).send('Error retrieving Firestore data');
+
+      });
+
+  });
+
+  app.get('/getstudent', async (req, res) => {
+
+    
+
+    const name = req.query.name
+
+    var dict = {};
+
+    const datesRef = db.collection('attendance');
+
+  const snapshot = await datesRef.orderBy(name).get();
+
+  if (snapshot.empty) {
+
+    res.status(200).send("We could not find " + name + " in our database.")
+    return;
+  }
+snapshot.forEach(doc => {
+  var date = doc.id 
+  const data = doc.data()
+  dict[date] = data[name]["status"]
+});
+res.status(200).send(dict)
+  });
